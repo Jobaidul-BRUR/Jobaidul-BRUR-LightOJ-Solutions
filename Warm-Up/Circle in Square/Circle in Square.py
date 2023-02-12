68a8a38b18570171540e776b565a031461804ada
Md. Jobaidul Islam <jobaidul.islam.432@gmail.com>
2023-02-13 02:08:29 +0600

    Create Circle in Square.py

---
+++ Warm-Up/Circle in Square/Circle in Square.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time
+from math import acos
+
+DEBUG = os.getenv("_DEBUG")
+if DEBUG:
+    input = open("cmake-build-debug/input.txt", "r").readline
+    # sys.stdout = open("output.txt", 'w')
+else:
+    input = sys.stdin.readline
+    output = sys.stdout.write
+
+
+def main(tc):
+    r = float(input())
+    print("%.2f" % ((r * r) * (4 - acos(-1))))
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    T = 1
+    T = int(input())
+    for tc in range(1, T + 1):
+        print("Case %d:" % tc, end=' ')
+        main(tc)
+    if DEBUG:
+        print("\n--- %0.0f ms ---" % ((time.time() - start_time) * 1000))
