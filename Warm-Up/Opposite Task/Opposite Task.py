2a5e004032ff648be0800642932aed959186555d
Md. Jobaidul Islam <jobaidul.islam.432@gmail.com>
2023-02-12 11:02:16 +0600

    Create Opposite Task.py

---
+++ Warm-Up/Opposite Task/Opposite Task.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import time
+
+DEBUG = os.getenv("_DEBUG")
+if DEBUG:
+    input = open("cmake-build-debug/input.txt", "r").readline
+    # sys.stdout = open("output.txt", 'w')
+else:
+    input = sys.stdin.readline
+    output = sys.stdout.write
+
+
+def main(tc):
+    n = int(input())
+    print((n + 1) // 2, n // 2)
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    T = 1
+    T = int(input())
+    for tc in range(1, T + 1):
+        main(tc)
+    if DEBUG:
+        print("\n--- %0.0f ms ---" % ((time.time() - start_time) * 1000))
